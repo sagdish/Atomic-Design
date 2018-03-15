d9711272dfbdd24e727b62c5fc832bcf40e072ed
Sagdi Formanov <forsy@Sagdis-MacBook-Pro.local>
2018-03-15 15:22:49 -0700

    setState

--- dogs/src/pages/Breeds.js
+++ dogs/src/pages/Breeds.js
@@ -20,6 +20,37 @@ class Breeds extends Component {
 
     return breeds.filter(breed => choiceBreeds[breed]);
   }
+  
+  
+  
+  render() {
+    return(
+      <div>
+        <Options title="Breeds" list={this.state.breeds} />
+      </div>
+          );
+  };
+  
+  
+  componentDidMount() {
+    axios.get('https://dog.ceo/api/breeds/list/all')
+    .then(response => {
+      console.log(response.data.message);
+      this.setState(
+        { breeds: this.filterBreeds(Object.keys(response.data.message)) });
+      console.log(this.state.breeds);
+    })
+    
+    
+    .catch(error => {
+      console.log(`error getting data from server: ${error}`);
+    });
+    
+    
+    
+    
+  }
+  
 }
 
 export default Breeds;
